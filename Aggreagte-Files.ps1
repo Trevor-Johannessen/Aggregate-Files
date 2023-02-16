f56bcebe1c7c41898c79f0eb252963b53f322643
Trevor Johannessen <91626575+Trevor-Johannessen@users.noreply.github.com>
2023-02-16 12:31:19 -0500

    Update Aggreagte-Files.ps1

--- Aggreagte-Files.ps1
+++ Aggreagte-Files.ps1
@@ -1,15 +1,27 @@
-#TODO 
-# - CHECK IF FILE ALREADY COPIED IS NEWER/OLDER THAN PREVIOUS
-# - IMPLEMENT VERBOSE FLAG TO PRINT OUTPUT
+$In
+$Out
+if(!$In){ $In = $args[0]}
+if(!$Out){$Out = $args[1]}
 
-$cmpFiles = Get-ChildItem -Path "Out" -File
-$srcFiles = Get-ChildItem -Recurse -Path "Test" -File
+
+$cmpFiles = Get-ChildItem -Path $out -File
+$srcFiles = Get-ChildItem -Recurse -Path $in -File
+$fileTable = @{}
 foreach($dir in $srcFiles){
     foreach($file in $dir){
-        $copy = $cmpFiles | Where-Object -Property "Name" -EQ $file.name
-        if($copy.LastWriteTime -lt $file.LastWriteTime){
+        If(-not $fileTable.ContainsKey($file.Name)){
+            $fileTable.Add($file.Name, @($file.PSPath, $file.LastWriteTime))
+        }ElseIf($fileTable[$file.Name][1] -lt $file.LastWriteTime){
             Write-Output $file.PSPath
-            Copy-Item -Path "$($file.PSPath)" -Destination "Out\\$($file.name)"
+            $fileTable[$file.Name] = @($file.PSPath, $file.LastWriteTime)
         }
+    }
+}
+
+foreach($file in $fileTable.keys){
+    Write-Output "File = $($file)"
+    $copy = $cmpFiles | Where-Object -Property "Name" -EQ $file
+    if($copy.LastWriteTime -lt $fileTable[$file][1]){
+        Copy-Item -Path $fileTable[$file][0] -Destination "$($out)\\$($file)"
     }
 }
